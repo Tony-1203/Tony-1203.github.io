50d00bdff14c0f05fa15465615d01d1bbf7e0b2e
Tony <2946568060@qq.com>
2025-05-28 15:59:17 +0800

    Updated API

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Supabase 配置
-const SUPABASE_URL = 'https://pvrhphegqewruutmdzpt.supabase.co'; 
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2cmhwaGVncWV3cnV1dG1kenB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzNTQ2NDksImV4cCI6MjA2MzkzMDY0OX0.5KJsUFmJwru8_DTC9npiwmMbZC8Mj95jZSWb6PQ1AuU'; // 替换为你的 Supabase Anon Key
+const SUPABASE_URL = 'https://xgfjhunnohnplxfkksen.supabase.co'; 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhnZmpodW5ub2hucGx4Zmtrc2VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0MTcyMTIsImV4cCI6MjA2Mzk5MzIxMn0.j10v6k7xzuz64NwMVtS1_xA-KjuOEoLIldWvsgU_0vw';
 
 // 初始化 Supabase 客户端
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
